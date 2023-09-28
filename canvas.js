9cc41e687170e862ad12882cc9fe8787cc288c4c
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-09-28 14:02:17 +0330

    Learn how to fill Rectangles

--- canvas.js
+++ canvas.js
@@ -3,7 +3,10 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
+
+c.fillStyle = "rgb(255, 141, 129)";
 c.fillRect(100, 100, 100, 100);
 c.fillRect(300, 100, 100, 100);
+c.fillStyle = "rgb(129, 137, 255)";
 c.fillRect(100, 300, 100, 100);
 c.fillRect(300, 300, 100, 100);
