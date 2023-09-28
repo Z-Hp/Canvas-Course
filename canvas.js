b9f6eeba3b41e41ccb2df3e4447ac167cb6b6792
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-09-28 14:02:58 +0330

    Learn how to draw Lines

--- canvas.js
+++ canvas.js
@@ -10,3 +10,11 @@ c.fillRect(300, 100, 100, 100);
 c.fillStyle = "rgb(129, 137, 255)";
 c.fillRect(100, 300, 100, 100);
 c.fillRect(300, 300, 100, 100);
+
+// Line
+c.beginPath();
+c.moveTo(100, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "rgb(117, 230, 124)";
+c.stroke();
