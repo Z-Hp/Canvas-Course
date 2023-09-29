d5940d84a03c88fdb2b249a2c47c2690fa13d81b
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-09-29 09:54:06 +0330

    Learn how to animate - part 1

--- canvas.js
+++ canvas.js
@@ -4,32 +4,58 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
-c.fillStyle = "rgb(255, 141, 129)";
-c.fillRect(100, 100, 100, 100);
-c.fillRect(300, 100, 100, 100);
-c.fillStyle = "rgb(129, 137, 255)";
-c.fillRect(100, 300, 100, 100);
-c.fillRect(300, 300, 100, 100);
+// c.fillStyle = "rgb(255, 141, 129)";
+// c.fillRect(100, 100, 100, 100);
+// c.fillRect(300, 100, 100, 100);
+// c.fillStyle = "rgb(129, 137, 255)";
+// c.fillRect(100, 300, 100, 100);
+// c.fillRect(300, 300, 100, 100);
 
-// Line
-c.beginPath();
-c.moveTo(100, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "rgb(117, 230, 124)";
-c.stroke();
+// // Line
+// c.beginPath();
+// c.moveTo(100, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "rgb(117, 230, 124)";
+// c.stroke();
 
-// Arc / Circle
+// // Arc / Circle
+// c.beginPath();
+// c.arc(600, 250, 100, 0, Math.PI * 2, false);
+// c.strokeStyle = "blue";
+// c.stroke();
+
+// for (var i = 0; i < 5; i++) {
+//   var x = Math.random() * window.innerWidth;
+//   var y = Math.random() * window.innerHeight;
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false);
+//   c.strokeStyle = "red";
+//   c.stroke();
+// }
+
 c.beginPath();
 c.arc(600, 250, 100, 0, Math.PI * 2, false);
 c.strokeStyle = "blue";
 c.stroke();
 
-for (var i = 0; i < 5; i++) {
-  var x = Math.random() * window.innerWidth;
-  var y = Math.random() * window.innerHeight;
+var x = 200;
+var y = 200;
+var dx = 4;
+var dy = 4;
+var radius = 30;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
   c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false);
-  c.strokeStyle = "red";
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
+  c.strokeStyle = "blue";
   c.stroke();
+
+  x += dx;
 }
+
+animate();
