704701e2940cfc370affc635ee02d409322ff62d
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-09-29 13:11:15 +0330

    Learn how to animate multiple circles
    How to create multiple circles that have their own independent X and Y values, independent velocities and independent radii so we can create multiple circles on-screen have them all go in different directions.

--- canvas.js
+++ canvas.js
@@ -37,31 +37,61 @@ var c = canvas.getContext("2d");
 //   c.stroke();
 // }
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 30;
-var dy = (Math.random() - 0.5) * 30;
-var radius = 30;
+// ----------------------------------------------------------------------------------------------------------
+// ------------------------------------ Create javaScript Object --------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
+function Circle(x, y, radius, dx, dy) {
+  this.radius = radius;
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+  };
+
+  this.update = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
+}
+// ---------------------------------------------- Create 100 Circles ----------------------------------------------------------
+
+var circleArray = [];
+for (var i = 0; i < 100; i++) {
+  var x = Math.random() * innerWidth;
+  var y = Math.random() * innerHeight;
+  // For dx and dy: By -0.5 we can have both positive and negative velocity & 3 is a factor to amplify the speed.
+  var dx = (Math.random() - 0.5) * 3;
+  var dy = (Math.random() - 0.5) * 3;
+  var radius = 30;
+
+  circleArray.push(new Circle(x, y, radius, dx, dy));
+}
+
+// ------------------------------------ Animate all the circles we create previously -----------------------------------------
 
 function animate() {
   requestAnimationFrame(animate);
 
   c.clearRect(0, 0, innerWidth, innerHeight);
-
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = "blue";
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-
-  x += dx;
-  y += dy;
 }
 
 animate();
+// --------------------------------------------------------------------------------------------------------------------------
